51bd365156b109280c8c5022536805032da1a54e
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-06-29 18:12:36 +0300

    add task06 45

---
+++ Task06_45/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+Console.Write("Введите элементы массива через пробел: ");
+int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Console.WriteLine(string.Join(" ", arr));
+
+void CopyArr(int[] array)
+{
+    Console.WriteLine("Копия заданного массива: ");
+    int[] copy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copy[i] = array[i];
+        Console.Write($"{copy[i]} ");
+    }
+    
+}
+
+CopyArr(arr);
